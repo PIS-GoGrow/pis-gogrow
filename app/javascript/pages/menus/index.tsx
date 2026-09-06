15d5ef73e285d0222da60733105ff55d8a499193
Juan Andrés Olmedo <olmedo.juanandres5@gmail.com>
2026-09-06 16:52:05 -0300

    Interfaz inicial para platos

--- app/javascript/pages/menus/index.tsx
+++ app/javascript/pages/menus/index.tsx
@@ -1,6 +1,18 @@
 import { Head, Link } from '@inertiajs/react'
+import { useState } from 'react'
+import { NewForm } from "./new.tsx"
 import type { Menu } from "@/types"
 import { menus as menusRoutes } from "@/routes"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 
 interface MenuProps {
   menus: Menu[]
@@ -8,16 +20,35 @@ interface MenuProps {
 
 export default function Index({ menus }: MenuProps) {
   const menusJSX = menus.map(menu =>
-	<div key={menu.id}>
-	  <p>{menu.name}</p>
-		<Link href={menusRoutes.show(menu.id)}> Ver </Link>
-	</div>
+	<Card key={menu.id} size="sm" className="w-full ">
+      <CardHeader>
+		<CardTitle>{menu.name}</CardTitle>
+        <CardDescription>
+           {menu.description}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{menu.price}$</p>
+      </CardContent>
+    </Card>
   )
+
+  const [showForm, setShowForm] = useState(false)
+
   return (
-    <div>
-      <h1>Tus platos</h1>
-	  <Link href={menusRoutes.new()}> Agegar plato </Link>
-      <div>{menusJSX}</div>
+    <div className="m-5 w-250 mx-auto grid gap-2">
+	  <div className="flex items-center mb-5">
+        <h1 className="text-lg font-bold">Tus platos</h1>
+		{ showForm ? (<> </>) : (
+		  <Button className="ml-auto" onClick={() => { setShowForm(true); }}>
+		  	Agregar plato
+		  </Button>
+		) }
+      </div>
+	  { showForm ? (
+		<NewForm onCancelar={() => { setShowForm(false); }} />
+	  ) : (<></>)}
+	  <div className="grid gap-2 grid-cols-2">{menusJSX}</div>
     </div>
   )
 }
--- app/javascript/pages/menus/new.tsx
+++ app/javascript/pages/menus/new.tsx
@@ -1,17 +1,59 @@
 import { Head, Link, Form } from '@inertiajs/react'
 import { menus as menusRoutes } from "@/routes"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function NewForm({ onCancelar }) {
+		return(
+		  <Card className="mx-auto w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Crear plato</CardTitle>
+        </CardHeader>
+				<Form action="/menus" method="post">
+          <CardContent>
+            <div className="flex flex-col gap-3">
+              <div className="grid gap-2">
+						    <Label htmlFor="name">Nombre</Label>
+				        <Input type="name" name="name" /> <br />
+							</div>
+
+              <div className="grid gap-2">
+						    <Label htmlFor="description">Descripción</Label>
+				        <Input type="description" name="description" /> <br />
+							</div>
+
+              <div className="grid gap-2">
+						    <Label htmlFor="price">Precio</Label>
+				        <Input type="price" name="price" /> <br />
+							</div>
+					  </div>
+		      </CardContent>
+		      <CardFooter className="flex-col gap-2">
+					  <Button type="submit" className="w-full"> Crear </Button>
+					  <Button onClick={() => { onCancelar(); }} variant="outline" className="w-full"> Cancelar </Button>
+          </CardFooter>
+		    </Form>
+      </Card>
+		)
+}
 
 export default function New() {
   return (
     <div>
 			<Head title="Agregar plato" />
-      <h1>Agregar plato</h1>
-			<Form action="/menus" method="post">
-				<input type="name" name="name" /> <br />
-				<input type="price" name="price" /> <br />
-				<input type="description" name="description" /> <br />
-				<button type="submit">Crear</button>
-			</Form>
+			
+		  <h1>Agregar plato</h1>
+		  <NewForm />
     </div>
   );
 }
