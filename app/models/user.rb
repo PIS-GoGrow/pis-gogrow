b25ba9c5a0df88853931fe2b068cd5a7ae4a5d1c
fedetournier <ftournierc@gmail.com>
2026-09-16 23:45:52 -0300

    fix(auth): denegar acceso sin perfil y corregir specs de roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,7 @@ class User < ApplicationRecord
 
   def self.find_from_google(auth)
     email = auth.info.email.to_s.downcase
+    raise DomainNotAllowed unless google_allowed_domains.include?(email.split("@").last)
 
     find_by email: email
   end
@@ -108,6 +109,7 @@ end
 #  google_uid      :string
 #  name            :string           not null
 #  password_digest :string           not null
+#  roles           :string           default([]), not null, is an Array
 #  verified        :boolean          default(FALSE), not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-ENV["RAILS_ENV"] ||= "test"
+ENV["RAILS_ENV"] = "test"
 require_relative "../config/environment"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
@@ -17,6 +17,7 @@ end
 
 RSpec.configure do |config|
   config.fixture_paths = [ Rails.root.join("spec/fixtures") ]
+  config.global_fixtures = :all
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
